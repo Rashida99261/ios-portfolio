e3ba2cbf5cfb5720a3d899a8212dfdb934699f4f
Rashida99261 <80286834+Rashida99261@users.noreply.github.com>
2026-09-23 18:15:41 +0530

    Add persistence layer module

---
+++ ios-persistence-layer/ios-persistence-layer/ios_persistence_layerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_persistence_layerApp.swift
+//  ios-persistence-layer
+//
+//  Created by Rashida on 23/09/26.
+//
+
+import SwiftUI
+
+@main
+struct ios_persistence_layerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
