3a066afa650addbaf11ff88b93f73666afef7134
Rashida99261 <80286834+Rashida99261@users.noreply.github.com>
2026-09-23 09:41:22 +0530

    Add SwiftUI and Swift UIKit components module

---
+++ ios-ui-components/ios-ui-components/ios_ui_componentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_ui_componentsApp.swift
+//  ios-ui-components
+//
+//  Created by Rashida on 23/09/26.
+//
+
+import SwiftUI
+
+@main
+struct ios_ui_componentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
